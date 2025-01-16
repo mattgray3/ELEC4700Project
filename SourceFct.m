bab898da04ceff1b8442a9d48df90483f3713174
Matthew Gray <mattgray3@ad.doe.carleton.ca>
2025-01-16 13:41:04 -0500

    SourceFct File

---
+++ SourceFct.m
@@ -0,0 +1,16 @@
+%ELEC  4700 Project Milestone 1 Source Function Matt Gray 101183570
+
+function E = SourceFct(t, InputParas)
+
+if isfield(InputParas, 'rep')
+    n = floor(t/InputParas.rep);
+    t = t - n*InputParas.rep;
+end
+
+if ~isstruct(InputParas)
+    E = InputParas;
+else
+    E = InputParas.E0 * exp(-(t-InputParas.t0)^2 / InputParas.wg^2)* ... 
+        exp(1i*(InputParas.we*t + InputParas.phi));
+end
+end
