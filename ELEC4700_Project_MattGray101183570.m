df9273754b6253b4035643b0549c9c3eb06eb5f7
Matthew Gray <mattgray3@ad.doe.carleton.ca>
2025-01-17 14:10:42 -0500

    Elec4700_Project_MattGray101183570 File

--- ELEC4700_Project_MattGray101183570.m
+++ ELEC4700_Project_MattGray101183570.m
@@ -1,4 +1,6 @@
 % Milestone 1 Matt Gray 101183570 ELEC 4700 Project 
+
+%Default setup for plotting
 set(0,'defaultaxesfontsize', 20)
 set(0, 'DefaultFigureWindowStyle', 'docked')
 set(0, 'DefaultLineLineWidth', 2)
@@ -6,57 +8,62 @@ set(0, 'Defaultaxeslinewidth', 2)
 
 set(0, 'DefaultFigureWindowStyle', 'docked')
 
-c_c = 299792458;
-c_eps_0 = 8.8542149e-12;
-c_eps_0_cm = c_eps_0/100; 
-c_mu_0 = 1/c_eps_0/c_c^2;
-c_q = 1.60217653e-19;
-c_hb = 1.05457266913e-34;
-c_h = c_hb*2*pi;
+% Constants used in Simulation
+c_c = 299792458; % Speed of light
+c_eps_0 = 8.8542149e-12; % eo
+c_eps_0_cm = c_eps_0/100; % eo in cm
+c_mu_0 = 1/c_eps_0/c_c^2; % uo 
+c_q = 1.60217653e-19; % electron charge
+c_hb = 1.05457266913e-34; %plancks constant
+c_h = c_hb*2*pi; %reduced plancks constant
 
+%Input parameters for L and R constants
 InputParasL.E0 = 1e5; %Adjusts the amplitude of the wave
 InputParasL.we = 0;
-InputParasL.t0 = 2e-12;
-InputParasL.wg = 5e-13;
+InputParasL.t0 = 2e-12; % inital time of the wave
+InputParasL.wg = 5e-13; %pusle width
 InputParasL.phi = 0;
-InputParasR = 0;
+InputParasR = 0; % right source equal to zero since only reflections come from right side
 
-n_g = 3.5;
-vg = c_c/n_g * 1e2;
-Lambda = 1550e-9;
+%Material and Simulation Parameters
+n_g = 3.5; % medium index
+vg = c_c/n_g * 1e2; % medium velocity 
+Lambda = 1550e-9; %wavelength
 
-RL = 0.9i;
+RL = 0.9i; % Reflectoin coefficients for L and R sides
 RR = 0.9i;
 
-plotN = 10;
+plotN = 10; % frequency of plotting
 
-L =  1000e-6 * 1e2;
-XL = [0, L];
+L =  1000e-6 * 1e2; %Len of sim
+XL = [0, L]; % X and Y Axis limits
 YL = [0, InputParasL.E0];
 
-Nz = 500;
-dz = L / (Nz-1);
-dt =  dz/vg;
-fsync = dt * vg /dz;
+Nz = 500; % # spatical grid pts
+dz = L / (Nz-1); % spacital step size
+dt =  dz/vg; % Time step size
+fsync = dt * vg /dz; % sync factor
 
-Nt =  floor(2*Nz);
-tmax = Nt * dt;
+Nt =  floor(2*Nz); % total # of time steps
+tmax = Nt * dt; % Max sim time
 t_L = dt * Nz;
 
-z = linspace(0, L, Nz);
+% Field Setup
+z = linspace(0, L, Nz); % Linear Spaced Vector
 time = nan(1,Nt);
 InputL = nan(1, Nt);
 InputR = nan(1, Nt);
 OutputL = nan(1, Nt);
 OutputR = nan(1, Nt);
 
-Ef = zeros(size(z));
-Er = zeros(size(z));
+Ef = zeros(size(z)); % Electric field forward
+Er = zeros(size(z));% Electric field Reflected
 
-Ef1 = @SourceFct;
+Ef1 = @SourceFct; % Generates source for electric field
 ErN = @SourceFct;
 
-t = 0;
+%Time Loop
+t = 0; % initalizing time of simulation
 time(1) = t;
 
 InputL(1) = Ef1(t, InputParasL);
@@ -68,6 +75,7 @@ OutputL(1) = Er(1);
 Ef(1) = InputL(1);
 Er(Nz) = InputR(1);
 
+% Setup For Simulation Figures
 figure('Name','Fields')
 subplot(3,1,1)
 plot(z*1000, real(Ef), 'r');
@@ -88,6 +96,8 @@ xlabel('time(ps)')
 ylabel('E')
 
 hold off
+
+%Main Time Stepping Loop iterativle increases time, soures and outputs
 for i = 2:Nt
     t = dt *(i-1);
     time(i) = t;
