e39ae64d6fdf1fd3d23193b77a7a681cdc5f2711
Matt Gray <matthewgray2074@gmail.com>
2025-04-03 12:44:09 -0400

    Add files via upload

---
+++ Milestone9NotesMG101183570.m
@@ -0,0 +1,25 @@
+
+%Milestone 9 ELEC 4700
+
+%-Chose to go about plotting the State Space representation
+
+%Tests
+%- Run as created in Milestone 8
+%- Adjust the betas AKA Detuning
+%- Adjust the grating
+%- Change the Length of the laser
+
+%What I Have Done So Far
+%- Tried breaking it with detuning
+%- System is stable, plot makes a loop
+
+%- Tried breaking with changing the length
+%- System is Staable with the plot comong back to the start
+
+%- Broke the System with grating
+%- Different Grating Setups provide different state space patterns
+
+%Next Steps
+%- Grating
+%- Change len
+%- Try breaking it
